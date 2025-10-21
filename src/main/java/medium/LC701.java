08363c9fc8ee766b86313c4b5a28d5ab90666e14
Daniel Silva <diielsilvasousa@gmail.com>
2025-10-21 13:32:22 -0300

    feat: adding LC701 answer

---
+++ src/main/java/medium/LC701.java
@@ -0,0 +1,43 @@
+package medium;
+
+class LC701 {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    TreeNode insertIntoBST(TreeNode root, int value) {
+        TreeNode aux = root;
+        TreeNode prev = null;
+
+        if (root != null) {
+            while (aux != null) {
+                prev = aux;
+
+                if (value > aux.val) {
+                    aux = aux.right;
+                } else {
+                    aux = aux.left;
+                }
+            }
+
+            if (value > prev.val) {
+                prev.right = new TreeNode(value);
+            } else {
+                prev.left = new TreeNode(value);
+            }
+        } else {
+            root = new TreeNode(value);
+        }
+
+
+        return root;
+    }
+}
+
